4e122a595f1a02db9d08b68bdd1f5c2369c6d20d
Kirill Zhiganov <zhigano_k.n@mail.ru>
2023-06-06 16:23:55 +0300

    Реализовал простенькую проверку. Не разбивал на компоненты и не делал рефакторинг

--- src/App.js
+++ src/App.js
@@ -9,15 +9,34 @@ function changeValue(target, setValue) {
   setValue(target.value);
 }
 
-function onSubmit(event, formData) {
-  event.preventDefault();
-  console.log(formData);
-}
-
 export const App = () => {
   const [emailValue, setEmailValue] = useState('');
   const [passwordValue, setPasswordValue] = useState('');
   const [secondaryPassword, setSecondaryPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  function resetSet() {
+    setEmailValue('');
+    setPasswordValue('');
+    setSecondaryPassword('');
+    setErrorMessage('');
+  }
+  function onSubmit(event) {
+    event.preventDefault();
+    let error = null;
+    if (!/^[\w]*$/.test(passwordValue)) {
+      error = 'Пароль должен содержать только буквы и цифры';
+      setErrorMessage(error);
+      return;
+    }
+    if (passwordValue !== secondaryPassword) {
+      error = 'Пароли не совпадают';
+      setErrorMessage(error);
+      return;
+    }
+    resetSet();
+    console.log({ email: emailValue, password: passwordValue });
+  }
 
   return (
     <div className={style.app}>
@@ -36,7 +55,7 @@ export const App = () => {
         <div className={style.flexBox}>
           <label htmlFor={'password'}>Введите пароль</label>
           <input
-            type="password"
+            type="text"
             name="password"
             id={'password'}
             value={passwordValue}
@@ -46,19 +65,21 @@ export const App = () => {
         <div className={style.flexBox}>
           <label htmlFor={'password-two'}>Повторите пароль</label>
           <input
-            type="password"
+            type="text"
             name="password-two"
             id={'password-two'}
             value={secondaryPassword}
             onChange={({ target }) => changeValue(target, setSecondaryPassword)}
           />
         </div>
-        <div className={style.errorMessage}></div>
+        <div className={style.errorMessage}>{errorMessage}</div>
         <div className={style.containerButton}>
           <button type={'submit'} className={`${style.button} ${style.buttonMain}`}>
             Зарегистрироваться
           </button>
-          <button className={`${style.button}`}>Сбросить</button>
+          <button type={'button'} className={style.button} onClick={resetSet}>
+            Сбросить
+          </button>
         </div>
       </form>
     </div>
